e24aff21a03738c64d879887677369cf2a60b1ec
OmegaRogue <thatomegarogue@gmail.com>
2020-05-23 17:41:32 +0200

    Add -y to apt-get

--- scripts/neovim_install.sh
+++ scripts/neovim_install.sh
@@ -1,3 +1,3 @@
-sudo apt-get install software-properties-common python-dev python-pip python3-dev python3-pip
+sudo apt-get install -y software-properties-common python-dev python-pip python3-dev python3-pip
 sudo apt-get update
-sudo apt-get install neovim
+sudo apt-get install -y neovim
