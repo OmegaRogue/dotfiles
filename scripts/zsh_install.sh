6cb342e557623d6a3c8a56a1a371482f6af816f4
OmegaRogue <omegarogue@omegavoid.codes>
2021-01-01 20:50:39 +0100

    ⏪ Move back to local antigen.zsh

--- scripts/zsh_install.sh
+++ scripts/zsh_install.sh
@@ -1,5 +1,7 @@
 sudo apt-get update
-sudo apt-get install -y zsh zsh-antigen
+sudo apt-get install -y zsh
+
+curl -L git.io/antigen > antigen/antigen.zsh
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
