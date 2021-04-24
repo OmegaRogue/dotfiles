293f863f8e57aa9e4e65345c3d6a175c6f486485
OmegaRogue <omegarogue@omegavoid.codes>
2021-04-24 23:51:10 +0200

    Autoconfirm package installs

--- scripts/neovim_install.sh
+++ scripts/neovim_install.sh
@@ -1,3 +1,3 @@
 if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache neovim
-elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install neovim
+elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install -y neovim
 else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: neovim">&2; fi
--- scripts/zsh_install.sh
+++ scripts/zsh_install.sh
@@ -1,3 +1,3 @@
 if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache zsh
-elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install zsh
+elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install -y zsh
 else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: zsh">&2; fi
