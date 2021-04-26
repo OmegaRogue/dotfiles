f802b8e6e42c5b34f9d27fcd969a4feaf65f94e9
OmegaRogue <omegarogue@omegavoid.codes>
2021-04-26 23:52:17 +0200

    Fix Arch support

--- scripts/zsh_install.sh
+++ scripts/zsh_install.sh
@@ -1,5 +1,5 @@
 if [ -x "$(command -v apk)" ];       then sudo apk add --no-cache zsh
 elif [ -x "$(command -v apt-get)" ]; then sudo apt-get install -y zsh
-elif [ -x "$(command -v yay)" ]; then yay --noconfirm zsh
+elif [ -x "$(command -v yay)" ]; then yay --noconfirm -S zsh
 elif [ -x "$(command -v pacman)" ]; then pacman -S --noconfirm zsh
 else echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: zsh">&2; fi
