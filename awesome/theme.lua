65fb1f52b686ac33213832fb6850e650040b56d2
OmegaRogue <omegarogue@omegavoid.codes>
2022-05-21 14:19:23 +0200

    Update

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -139,7 +139,7 @@ end
 theme.menu_submenu_icon = themes_path .. "default/submenu.png"
 
 theme.menu_height = dpi(30)
-theme.menu_width = dpi(200)
+theme.menu_width = dpi(250)
 
 theme.wibar_height = dpi(30)
 
